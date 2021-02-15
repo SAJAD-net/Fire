63afd268f2905ca2dfc49cdb8515b5c3f19d6f94
sajad-net <sajadsajad.sch@gmail.com>
2021-02-15 05:47:52 -0500

    updated a fire app

--- app/fire.py
+++ app/fire.py
@@ -12,9 +12,15 @@ ap=argparse.ArgumentParser()
 ap.add_argument("-n","--new",nargs='?' ,required=False,help="Initializing ...")
 ap.add_argument("-f","--freeze",nargs='?' ,required=False,help="Freezing modules")
 ap.add_argument("-i","--install",nargs='?' ,required=False,help="Installing modules")
-ap.add_argument("-p","--python",default="3",required=False,help="Install for one version of python")
+ap.add_argument("-p","--python",default="3",required=False,help="working on this version of python")
 ap.add_argument("-c","--count",required=False,help="Count of modules to install")
 ap.add_argument("-a","--auto",nargs='?' ,required=False,help="Auto freezing and installing modules")
+ap.add_argument("-m","--modules",nargs='?' ,required=False,help="Print a number of your modules")
+ap.add_argument("-u","--upgrade",nargs='?' ,required=False,help="Upgrade all modules")
+ap.add_argument("-v","--version",nargs='?' ,required=False,help="Print a version of fire app")
+
+
+
 args=vars(ap.parse_args())
 
 #fine, this is a path variable of program folder 
@@ -31,45 +37,43 @@ def Help():
 	print("""
 	1- -n --> Initializing ...
 	2- -f --> Freezing Modules 
-	3- -i --> installing Modules 
-	4- -i -c 20 installing 20 Modules only
-	5- -f -i -c 20 -->  Freezing and installing 20 Modules only
-	6- -p --> install module for one version of python 
-	7- -a --> auto freezing and installing modules
+	3- -m --> number of your modules
+	4- -i -p --> installing Modules for this version of python
+	5- -i -c --> 20 installing 20 Modules only
+	6- -f -i -c 20 -->  Freezing and installing 20 Modules only
+	7- -p --> working on this version of python 
+	8- -a -p 3 --> auto freezing and installing all modules\n\tfor this version of python 
+	9- -u --> upgrade all modules
+	10- -v --> version of fire app
 
 Example :
 	fire [command]:
 	[+]- fire -n --> Initializing ...
-	[+]- fire -a -p 3 -c 10 --> Freezing and inastalling 10\n\t\tmodule for python3	
+	[+]- fire -m --> number of your modudles is --> 
+	[+]- fire -a -p 3 -c 10 --> Freezing and inastalling 10\n\tmodule for python3	
 	""")
 
 #this function is intitalizer
 def Initialize():
 	if os.path.exists("%s/.fire"%home):
 		if os.path.isfile("%s/.fire/logs"%home) and os.path.isfile("%s/.fire/Modules"%home):
-			print("you are also initialized ... :)")
+			print("[!]- you are also initialized ... :)")
 	else:
-		print("WELCOME TO fire :)")
-		print("Initializing ...")
-		print("Please wite ...")
-		sleep(2)
-
+		print("[+]- Initializing ...")
 		os.system("mkdir $HOME/.fire >> $HOME/.fire/logs")
 		os.system("touch $HOME/.fire/logs >> $HOME/.fire/logs;touch $HOME/.fire/Modules >> $HOME/.fire/logs")
 		#os.system("echo $HOME > .initialize")
-		print("Initializing Finished ! :)")
+		print("[+]-Initializing Finished ! :)")
 
 #this function is modules freezer
 def Freezer():
-	print("WELCOME To fire :)")
-	print("Freezing a modules ...")
-	print("Please wite ...")
+	print("[+]- Freezing a modules ...")
 	os.system("echo Freezing >> %s/logs;date >> %s/logs;cp %s/Modules %s/Modules.backup >> %s/logs;python%s -m pip freeze > Modules;mv Modules %s/Modules"%(path,path,path,path,path,args["python"],path))
-	print("Freezing Finished ! :)")
+	print("[+]- Freezing Finished ! :)")
 
 #this function is modules intaller 
 def Installer():
-	print("Strating installation ...")
+	print("[+]- Strating installation ...")
 	if "-c" in sys.argv[:]:
 		scope=sys.argv[:].index("-c")
 		scope=sys.argv[scope+1]
@@ -80,9 +84,11 @@ def Installer():
 						os.system("echo Installing >> %s/logs;date >> %s/loga;python%s -m pip install %s >> %s/logs"%(path,path,args["python"],line,path))
 						count+=1
 					except:
-						print("Error : Please Checking internet and help")
+						print("[!]- Error : Please Checking internet connection and help")
 						file.close()
 						Help()
+		file.close()			
+
 	else:
 		with open("%s/.fire/Modules"%(home),"r") as file:
 			for line in file.readlines():
@@ -90,10 +96,21 @@ def Installer():
 					os.system("echo Intsalling >> %s/logs;date >> %s/logs;python%s -m pip install %s >> %s/logs"%(path,path,args["python"],line,path))
 					count+=1
 				except:
-					print("Error : Please Checking help")
+					print("[!]- Error : Please Checking help")
 					file.close()
 					Help()
 					break
+		file.close()
+#fine, this is main function for manage and run functions
+def modules():
+	num = 0
+	with open("%s/.fire/Modules"%(home),"r") as modules:
+		for line in modules:
+			num += 1
+	print("[+]- number of your modules is --> %i"%num)
+	modules.close()
+def version():
+	print("[+]- Fire version 2.0 ")
 #fine, this is main function for manage and run functions
 def RUN():
 	if "-n" in sys.argv[:]:# this is run the initialize function
@@ -105,8 +122,12 @@ def RUN():
 	elif "-a" in sys.argv[:]: # this is auto first run freeze function and second run installer function
 		Freezer()
 		Installer()
+	elif "-m" in sys.argv[:]:
+		modules()
+	elif "-v" in sys.argv[:]:
+		version()
 	else:
-		print("Error : Please enter following argument to works",end="")
+		print("[!]- Error : Please enter following argument to works",end="")
 		Help()
 
 #in following line, called run function for run program
